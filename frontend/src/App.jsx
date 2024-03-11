ca72dd88012004a9bf2f9dc9b4c81011b66d0ed4
keinermendoza <keinermendoza@gmail.com>
2024-03-11 09:58:18 -0400

    starting routing creation proccess

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,8 +16,18 @@ import {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
-      {/* <Route path="dashboard" element={<Dashboard />} /> */}
-      {/* <Route path="about" element={<About />} /> */}
+      {/* <Route path="monedero" element={<MonederoLayout />}>
+        <Route path="resumen" element={<Resumen />} />
+        <Route path="gastos" element={<Gastos />} />
+        <Route path="ingresos" element={<Ingresos />} />
+        <Route path="deudas" element={<Deudas />} />
+        <Route path="ahorro" element={<Ahorro />} />
+      </Route>
+      <Route path="inicio" element={<Inicio />} />
+      <Route path="perfil" element={<Perfil />} />
+      <Route path="entrar" element={<Entrar />} />
+      <Route path="registro" element={<Registro />} /> */}
+
     </Route>
   )
 );
